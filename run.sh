2cf42e2637183c918b5a7aef3dc080c6b8b6c8bf
Gareth Rees <grees@azuron.co.uk>
2017-01-25 09:54:48 +0000

    helloworld

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-ansible all -i './hosts/inventory' -m ping --private-key='~/.ssh/ansiblemaster' -u ec2-user -vvvv
+#ansible all -i './hosts/inventory' -m ping --private-key='~/.ssh/ansiblemaster' -u ec2-user -vvvv
+ansible-playbook -i './hosts/inventory' --private-key='~/.ssh/ansiblemaster' -u ec2-user ./playbooks/helloworld.yml
