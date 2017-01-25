e818690fc70b91856500bc7c9e452a07f0a8b15c
Gareth Rees <grees@azuron.co.uk>
2017-01-25 11:55:28 +0000

    helloworld

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #ansible all -i './hosts/inventory' -m ping --private-key='~/.ssh/ansiblemaster' -u ec2-user -vvvv
-ansible-playbook -i './hosts/inventory' --private-key='~/.ssh/ansiblemaster' -u ec2-user ./playbooks/helloworld.yml
+ansible-playbook -i './hosts/inventory' --private-key='~/.ssh/ansiblemaster' --connection=local ./playbooks/helloworld.yml
